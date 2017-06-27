898aa12b60589c9e62c641c69b33164f9d506fe5
macbookpro <jason@loblein.com>
2017-06-27 16:36:58 -0400

    add necessary gems for deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,15 +16,24 @@ config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'protected-oasis-54280.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+
+  config.action_mailer.smtp_settings = {
+    :address          => 'smtp.gmail.com',
+    :port             => 587,
+    :user_name        => ENV['gmail_username'],
+    :password         => ENV['gmail_password'],
+    :authentication   => 'plain',
     :enable_starttls_auto => true
   }
+  # ActionMailer::Base.smtp_settings = {
+  #   :address        => 'smtp.sendgrid.net',
+  #   :port           => '587',
+  #   :authentication => :plain,
+  #   :user_name      => ENV['SENDGRID_USERNAME'],
+  #   :password       => ENV['SENDGRID_PASSWORD'],
+  #   :domain         => 'heroku.com',
+  #   :enable_starttls_auto => true
+  # }
 
   config.serve_static_assets = true
 
