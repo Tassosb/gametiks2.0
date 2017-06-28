3ab3c00d14d6f63ac9b3a376819f9fb59d55aca2
macbookpro <jason@loblein.com>
2017-06-28 09:05:41 -0400

    update action mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,12 +16,10 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  host = 'gametiks.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
 
   ActionMailer::Base.smtp_settings = {
     :address          => 'smtp.sendgrid.net',
-    :domain           => 'gametiks.herokuapp.com',
+    :domain           => 'heroku.com',
     :port             => 587,
     :user_name        => ENV['SENDGRID_USERNAME'],
     :password         => ENV['SENDGRID_PASSWORD'],
