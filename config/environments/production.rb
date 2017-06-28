997587411061c9a32cd53c96552302720d269862
macbookpro <jason@loblein.com>
2017-06-28 09:11:41 -0400

    add hostname

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,8 +15,7 @@ Rails.application.configure do
   # Configure Action Mailer using SendGrid
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-
+  config.action_mailer.default_url_options = { host: 'gametiks.herokuapp.com' }
   ActionMailer::Base.smtp_settings = {
     :address          => 'smtp.sendgrid.net',
     :domain           => 'heroku.com',
