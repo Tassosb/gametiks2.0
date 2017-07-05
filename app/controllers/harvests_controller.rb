55828e515f824fa2de2554622814747ef607ea9a
macbookpro <jason@loblein.com>
2017-07-05 19:30:40 -0400

    remove unnecessary comments

--- app/controllers/harvests_controller.rb
+++ app/controllers/harvests_controller.rb
@@ -1,81 +1,54 @@
 class HarvestsController < ApplicationController
-  before_action :set_harvest, only: [:show, :edit, :update, :destroy]
 
-  # GET /harvests
-  # GET /harvests.json
   def index
     @harvests = Harvest.all
   end
 
-  # GET /harvests/1
-  # GET /harvests/1.json
   def show
-    @harvest = Harvest.find(params[:id])
   end
 
-  # GET /harvests/new
   def new
     @harvest = Harvest.new
     @user = current_user
-
     gon.userLat = @user.latitude;
     gon.userLng = @user.longitude;
   end
 
-  # GET /harvests/1/edit
   def edit
   end
 
-
-
-  # POST /harvests
-  # POST /harvests.json
   def create
     @harvest = Harvest.new(harvest_params)
     @harvest.user_id = current_user.id
-    respond_to do |format|
-      if @harvest.save
-        flash[:success] = "Harvest successfully created"
-        format.html { redirect_to current_user }
-        format.json { render :show, status: :created, location: @harvest }
-      else
-        render :new
-      end
+    if @harvest.save
+      flash[:success] = "Harvest successfully created"
+      redirect_to current_user
+    else
+      render @harvest.errors.full_messages
     end
   end
 
-  # PATCH/PUT /harvests/1
-  # PATCH/PUT /harvests/1.json
   def update
     respond_to do |format|
       if @harvest.update(harvest_params)
-        format.html { redirect_to @harvest }
-        format.json { render :show, status: :ok, location: @harvest }
+        redirect_to current_user
       else
-        format.html { render :edit }
-        format.json { render json: @harvest.errors, status: :unprocessable_entity }
+        render @harvest.errors.full_messages
       end
     end
   end
 
-  # DELETE /harvests/1
-  # DELETE /harvests/1.json
   def destroy
     @harvest.destroy
     respond_to do |format|
-      format.html { redirect_to harvests_url, notice: 'Harvest was successfully destroyed.' }
-      format.json { head :no_content }
+      redirect_to user_url, notice: 'Harvest was successfully destroyed.'
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_harvest
-      @harvest = Harvest.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def harvest_params
-      params.require(:harvest).permit(:weapon_type, :animal_type, :weight, :description, :image, :latitude, :longitude)
-    end
+  def harvest_params
+    params.require(:harvest).permit(:weapon_type, :animal_type, :weight, :description, :image, :latitude, :longitude)
+  end
+  
 end
--- app/models/harvest.rb
+++ app/models/harvest.rb
@@ -1,24 +1,21 @@
 class Harvest < ActiveRecord::Base
   belongs_to :user
-  
+
    # This method associates the attribute ":image" with a file attachment
   has_attached_file :image, styles: {
     thumb: '180x180#'
   }
 
-  # validates :animal_type, :presence => { :if => 'animal_type.nil?' }
-  
   # Validate the attached image is image/jpg, image/png, etc
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-  
-  
+
   # CarrierWave gem uploader
-  # mount_uploader :image, ImageUploader
-  
-  # validates :animal_type, presence: true
-  # validates :weapon_type, presence: true
-  # validates :image, presence: true
-  
-   
-  
+  mount_uploader :image, ImageUploader
+
+  validates :animal_type, presence: true
+  validates :weapon_type, presence: true
+  validates :image, presence: true
+
+
+
 end
