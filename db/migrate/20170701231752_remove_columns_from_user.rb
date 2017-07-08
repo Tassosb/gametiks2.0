eb2490856502f14f2ee4097358d34f6ca94f7afa
macbookpro <jason@loblein.com>
2017-07-08 12:28:52 -0400

    remove field

--- db/migrate/20170701231752_remove_columns_from_user.rb
+++ db/migrate/20170701231752_remove_columns_from_user.rb
@@ -1,6 +1,5 @@
 class RemoveColumnsFromUser < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :encrypted_password 
     remove_column :users, :encrypted_password
     remove_column :users, :reset_password_token
     remove_column :users, :reset_password_sent_at
