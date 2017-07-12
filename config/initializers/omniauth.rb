136b2214a31f99689ccca592d54bd40a0228835a
macbookpro <jason@loblein.com>
2017-07-12 15:54:30 -0400

    change omniauth callback url

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,8 @@
 #OmniAuth.config.logger = Rails.logger
-
-#Rails.application.config.middleware.use OmniAuth::Builder do
+# 
+# Rails.application.config.middleware.use OmniAuth::Builder do
 # provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_AP_SECRET'],
-#   callback_url: 'http://localhost:3000/auth/facebook/callback'
-#     scope: 'name, email', display: 'popup'
-#   end
+#   callback_url: 'https://gametiks.herokuapp.com/users/auth/facebook/callback'
+#     # scope: 'name, email', display: 'popup'
+#   # end
 # end
