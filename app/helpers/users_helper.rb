c0ce3aee1a333adb53823d36079b2c027c797a8d
macbookpro <jason@loblein.com>
2017-07-22 20:09:44 -0400

    fix switchable windows

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -61,7 +61,7 @@ def count_harvests(user)
     moose: 0,
     canine: 0,
     water_fowl: 0,
-    misc: 0
+    miscellaneous: 0
   }
 
   user.harvests.each do |harvest|
--- app/models/harvest.rb
+++ app/models/harvest.rb
@@ -1,20 +1,10 @@
 class Harvest < ActiveRecord::Base
   belongs_to :user
 
-   # This method associates the attribute ":image" with a file attachment
-  # has_attached_file :image, styles: {
-  #   thumb: '180x180#'
-  # }
-
-  # Validate the attached image is image/jpg, image/png, etc
-  # validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-
   # CarrierWave gem uploader
   mount_uploader :image, HarvestUploader
 
   validates :animal_type, :weapon_type, :weight, :image, :latitude, :longitude, presence: true
-  # validates :weapon_type, presence: true
-  # validates :image, presence: true
 
   def save_points
     self.user.save_points
