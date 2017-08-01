06114e50818fb3d3e34a3ca247c8b9b5c0ac7009
macbookpro <jason@loblein.com>
2017-08-01 13:10:13 -0400

    add main map

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,15 @@
 class StaticPagesController < ApplicationController
   def home
+    allHarvests = Harvest.includes(:user).all
+    gon.allHarvests = allHarvests.to_json(include: [ :user ])
   end
 
   def help
   end
-  
+
   def about
   end
-  
+
   def contact
   end
 end
