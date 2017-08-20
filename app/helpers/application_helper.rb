3f6cee91e8d94586374cdcd27caa7f2aa4c752e6
macbookpro <jason@loblein.com>
2017-08-20 17:31:57 -0400

    shorten base title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "Gametiks: Official Tally for Wild Game Harvest"
+    base_title = "Gametiks"
     if page_title.empty?
       base_title
     else
