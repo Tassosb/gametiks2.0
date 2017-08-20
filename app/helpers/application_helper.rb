a4d858150ce6689ef7bcb4fef663d551120de427
macbookpro <jason@loblein.com>
2017-08-20 18:40:57 -0400

    revise meta data

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "Gametiks"
+    base_title = "Gametiks | Wild Game Logger"
     if page_title.empty?
       base_title
     else
