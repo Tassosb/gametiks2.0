80a6793771b5c74479901dc0c1d6892a3b80b2ae
macbookpro <jason@loblein.com>
2017-08-24 09:11:15 -0400

    delete unused files and replace default mail url

--- config/initializers/omniauth.rb
+++
@@ -1,8 +0,0 @@
-#OmniAuth.config.logger = Rails.logger
-# 
-# Rails.application.config.middleware.use OmniAuth::Builder do
-# provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_AP_SECRET'],
-#   callback_url: 'https://gametiks.herokuapp.com/users/auth/facebook/callback'
-#     # scope: 'name, email', display: 'popup'
-#   # end
-# end
