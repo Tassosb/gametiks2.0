382a5d01f76acb4b8633a4c48cc4803d97a6f4df
macbookpro <jason@loblein.com>
2017-08-31 20:36:02 -0400

    fix migration bug

--- db/migrate/20170831235459_remove_null_constraint.rb
+++ db/migrate/20170831235459_remove_null_constraint.rb
@@ -1,6 +1,4 @@
 class RemoveNullConstraint < ActiveRecord::Migration[5.0]
   def change
-    change_column_null(:credits, :created_at, true )
-    change_column_null(:credits, :updated_at, true )
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 201708251555305) do
+ActiveRecord::Schema.define(version: 20170831235459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
