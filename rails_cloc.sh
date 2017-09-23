e2f6b92274805a97343111a6bc8bae51044d2076
macbookpro <jason@loblein.com>
2017-09-23 11:04:56 -0400

    add facebook sharing

---
+++ rails_cloc.sh
@@ -0,0 +1,1 @@
+find . \( -iname '*.rb' -o -iname '*.css' -o -iname '*.js' -o -iname '*.erb' -o -iname '*.html' -o -iname '*.haml' -o -iname '*.scss' -o -iname '*.coffee' \) -exec wc -l {} + | sort -n
