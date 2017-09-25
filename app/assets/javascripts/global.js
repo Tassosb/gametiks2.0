c8d531d5b561badf3ed8f0507f14d72f3d388a72
macbookpro <jason@loblein.com>
2017-09-25 12:14:14 -0400

    add fake char to change git

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -3,11 +3,11 @@ $(document).ready(function() {
   var associatedViews = $('.spawn-monitor-fill');
   for (var i = 0; i < associatedViews.length; i++) {
     // grab the child element with id = monitor-fill-container
-    const child = '<div class="monitor-fill-container"></div>';
-    const childNode = $.parseHTML(child);
+    var child = '<div class="monitor-fill-container"></div>';
+    var childNode = $.parseHTML(child);
     $(associatedViews[i]).prepend(childNode);
-    const height = $(childNode).css('height');
-    var multiplier = Math.round(parseInt(height.substring(0, height.length - 2)) / 2);
+    var height = $(childNode).css('height');
+    varb multiplier = Math.round(parseInt(height.substring(0, height.length - 2)) / 2);
     for (var j = 0; j < multiplier; j++) {
       const fill = $.parseHTML(`<div id="fill-${j}" class="monitor-fill"></div>`);
       $(childNode).append(fill);
